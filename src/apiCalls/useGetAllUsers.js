07bf2b0679a4e2557268e542f5c422e6511b25fe
reza heshmati <6510361-rezaheshmatidev@users.noreply.gitlab.com>
2022-04-18 13:56:06 +0430

    create useGetAllUsers hook

--- src/apiCalls/useGetAllUsers.js
+++ src/apiCalls/useGetAllUsers.js
@@ -2,12 +2,37 @@ import { useQuery } from "react-query";
 import { getToken } from "../utils/getToken";
 import instance from "./instance";
 
-const getAllUsers = () => {
-  return instance.get("/users");
+const getAllUsers = ({ keyword, offset, limit, sortData }) => {
+  console.log({ sortData });
+  return instance.get("/users", {
+    params: {
+      limit,
+      offset,
+      ...(keyword
+        ? {
+            val_fast_search: keyword,
+            type_sort: sortData.type || "asc",
+            type_search: "fast",
+            field_fast_search:
+              sortData.field == "firstName"
+                ? "users.first_name,users.last_name,users.mobile,users.email"
+                : "users.last_name,users.first_name,users.mobile,users.email",
+          }
+        : {
+            keyword: "",
+            type_sort: sortData.type || "asc",
+            type_search: "fast",
+            field_fast_search:
+              sortData.field == "firstName"
+                ? "users.first_name,users.last_name,users.mobile,users.email"
+                : "users.last_name,users.first_name,users.mobile,users.email",
+          }),
+    },
+  });
 };
 
-const useGetAllUsers = () => {
-  return useQuery("users", getAllUsers, {
+const useGetAllUsers = (params) => {
+  return useQuery("users", () => getAllUsers(params), {
     enabled: getToken() ? true : false,
   });
 };
